18fed1efc0ebf4f9a796bb7c577ec3ac99c6d626
Ahmed Hachemi <ahmedhachemi1994@gmail.com>
2025-02-13 15:45:51 +0100

    update cicd

--- src/test/java/com/example/dvfexplorer/DvfExplorerApplicationTests.java
+++ src/test/java/com/example/dvfexplorer/DvfExplorerApplicationTests.java
@@ -3,8 +3,8 @@ package com.example.dvfexplorer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DvfExplorerApplicationTests {
+@SpringBootTest(classes = DvfExplorerApplication.class)
+public class DvfExplorerApplicationTests {
 
     @Test
     void contextLoads() {
