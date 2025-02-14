3c70ca592b2759339c092f0b67138acd1cad3afc
Ahmed Hachemi <ahmedhachemi1994@gmail.com>
2025-02-14 13:16:03 +0100

    hotfix rename folder

--- src/main/java/com/example/dvfexplorer/DvfExplorerApplication.java
+++ src/main/java/com/example/dvfexplorer/DvfExplorerApplication.java
@@ -2,8 +2,14 @@ package com.example.dvfexplorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.dvfexplorer.model")
+@ComponentScan(basePackages = "com.example.dvfexplorer")
+@EnableJpaRepositories(basePackages = "com.example.dvfexplorer.repository")
 public class DvfExplorerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DvfExplorerApplication.class, args);
